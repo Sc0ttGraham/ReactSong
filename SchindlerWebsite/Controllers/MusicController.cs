64cd9f4d2b9ae38b36138755fa2872ee8a70b125
Scott Graham <sukohamu@gmail.com>
2016-08-16 19:12:25 -0400

    Beginning Stages

---
+++ SchindlerWebsite/Controllers/MusicController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchindlerWebsite.Controllers
+{
+    public class MusicController : Controller
+    {
+        public ActionResult React()
+        {
+            return View();
+        }
+    }
+}
